8c4a071c3dc755810a82905db97ecae7f20a49e5
Andrew Marabante <andrewmarabante@gmail.com>
2023-06-26 16:26:16 -0700

    Added a delete button and function  that deletes the last entry.

--- calc.js
+++ calc.js
@@ -1,6 +1,8 @@
 let num1 = null;
 let num2 = null;
 const newNum = document.querySelectorAll(".number");
+const deletebutt = document.getElementById("delete")
+deletebutt.addEventListener('click', deleteNum)
 newNum.forEach(number =>
     {
         number.addEventListener('click', setNum)
@@ -13,4 +15,16 @@ function setNum(e){
     }
     else if (num2 === null){num2 = this.id; console.log(num2)}
     else{console.log('only two nums!')}  
+}
+
+function deleteNum(){
+    if (num2 != null)
+    {
+        num2 = null;
+    }
+    else if (num1 != null)
+    {
+        num1 = null;
+    }
+    else{ console.log('nothing to delete')}
 }
